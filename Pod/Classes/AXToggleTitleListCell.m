13819a144222c09db4efb196e92c04cc83798418
Hiroki Akiyama <aki-hiroki@nifty.com>
2015-05-08 13:17:03 +0900

    cel showing

--- Pod/Classes/AXToggleTitleListCell.m
+++ Pod/Classes/AXToggleTitleListCell.m
@@ -28,22 +28,37 @@
   self.tintColor = [UIColor colorWithWhite:1.0 alpha:0.2];
 
   CGRect labelBounds = self.textLabel.bounds;
-  CGRect frame = CGRectOffset(labelBounds,
-                              self.separatorInset.left,
-                              CGRectGetHeight(self.bounds) - CGRectGetHeight(labelBounds) - 8.0);
-  [self.textLabel setFrame:frame];
+  CGRect textFrame = CGRectOffset(labelBounds,
+                                  self.separatorInset.left,
+                                  CGRectGetHeight(self.bounds) - CGRectGetHeight(labelBounds) - 8.0);
   
   // Accessory view
+  UIButton *accessoryButton;
   for (UIView *button in self.subviews) {
     if ([button isKindOfClass:[UIButton class]]) {
+      accessoryButton = (id)button;
       button.center = (CGPoint){
         CGRectGetWidth(self.bounds) - CGRectGetWidth(button.frame) - 8.0,
-        self.textLabel.center.y//CGRectGetHeight(self.bounds) - CGRectGetHeight(button.frame)
+        CGRectGetMidY(textFrame)//CGRectGetHeight(self.bounds) - CGRectGetHeight(button.frame)
       };
+      
       button.tintColor = self.textLabel.textColor;
       break;
     }
   }
+  
+  // Text label
+  if (accessoryButton) {
+    textFrame.size.width = (CGRectGetMinX(accessoryButton.frame) -
+                            self.separatorInset.left -
+                            8.0);
+  } else {
+    textFrame.size.width = MIN(textFrame.size.width,
+                               CGRectGetWidth(self.bounds) -
+                               self.separatorInset.left -
+                               8.0);
+  }
+  [self.textLabel setFrame:textFrame];
 }
 
 - (void)configureULSSubviewListCell
